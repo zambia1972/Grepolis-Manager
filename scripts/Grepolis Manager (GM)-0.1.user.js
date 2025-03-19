687b339b27ae8502a2258c7ddc8efbf1d62cb688
zambia1972 <qatan.1972@gmail.com>
2025-03-19 20:21:33 +0100

    Update Grepolis Manager (GM)-0.1.user.js

--- scripts/Grepolis Manager (GM)-0.1.user.js
+++ scripts/Grepolis Manager (GM)-0.1.user.js
@@ -1,4 +1,4 @@
-// ==UserScript==
+00// ==UserScript==
 // @name         Grepolis Manager
 // @namespace    http://tampermonkey.net/
 // @version      0.1
@@ -873,8 +873,8 @@
                         grid-template-columns: repeat(5, 1fr);
                         gap: 5px;
                         position: fixed; /* Gebruik 'fixed' voor een vaste positie */
-                        bottom: 20px; /* 20px vanaf de onderkant van het venster */
-                        right: 20px; /* 20px vanaf de rechterkant van het venster */
+                        bottom: 220px; /* 20px vanaf de onderkant van het venster */
+                        right: 220px; /* 20px vanaf de rechterkant van het venster */
                         z-index: 9999;
                         background: #f5f5f5;
                         padding: 10px;
