469a413da07d2cd98f3067a7c61081f91bd2be5e
zambia1972 <qatan.1972@gmail.com>
2025-05-30 21:34:01 +0200

    Update main.js

--- core/main.js
+++ core/main.js
@@ -1,2 +1,28 @@
-// Initiele setup, start UI, en initialisatie van modules
-console.log('Grepolis Manager geladen');
+// core/main.js
+
+(function () {
+  'use strict';
+
+  console.log('[Grepolis Manager] gestart.');
+
+  // Voorbeeld initialisatie van modules (moet aangepast naarmate modules klaar zijn)
+  if (typeof ForumManager !== 'undefined') {
+    const forumManager = new ForumManager();
+    console.log('[GM] ForumManager actief.');
+  }
+
+  if (typeof attackRangeHelperInit === 'function') {
+    attackRangeHelperInit();
+    console.log('[GM] AttackRange Helper geladen.');
+  }
+
+  if (typeof feestenFixedInit === 'function') {
+    feestenFixedInit();
+    console.log('[GM] FeestenFixed geladen.');
+  }
+
+  if (typeof setupGrepoChatInterface === 'function') {
+    setupGrepoChatInterface();
+    console.log('[GM] GrepoChat interface geactiveerd.');
+  }
+})();
