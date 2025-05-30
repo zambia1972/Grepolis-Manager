cbca60de59cd950ad2f54fb982b0a450eafe5499
zambia1972 <qatan.1972@gmail.com>
2025-05-30 21:41:38 +0200

    Update feestenFixedHelper.js

--- modules/helpers/feestenFixedHelper.js
+++ modules/helpers/feestenFixedHelper.js
@@ -1,1 +1,13 @@
-// FeestenFixed helpermodule
+// modules/helpers/feestenFixedHelper.js
+
+(function () {
+  'use strict';
+
+  function feestenFixedInit() {
+    console.log('[GM] FeestenFixed Helper geactiveerd');
+    // Hier kun je logica toevoegen om beschikbare feesten in steden te tonen
+  }
+
+  window.feestenFixedInit = feestenFixedInit;
+})();
+
