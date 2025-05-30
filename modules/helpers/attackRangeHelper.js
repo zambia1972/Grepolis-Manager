2f936ffe29722534632597cb267e4da0bc6ac1f3
zambia1972 <qatan.1972@gmail.com>
2025-05-30 21:40:49 +0200

    Update attackRangeHelper.js

--- modules/helpers/attackRangeHelper.js
+++ modules/helpers/attackRangeHelper.js
@@ -1,1 +1,12 @@
-// Aanvalsbereik helpermodule
+// modules/helpers/attackRangeHelper.js
+
+(function () {
+  'use strict';
+
+  function attackRangeHelperInit() {
+    console.log('[GM] AttackRange Helper geactiveerd');
+    // Hier kan logica komen om steden te markeren of bereik visueel aan te duiden
+  }
+
+  window.attackRangeHelperInit = attackRangeHelperInit;
+})();
