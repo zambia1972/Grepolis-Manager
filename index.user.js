4a4ad0d7bd4b00129d51765d4077e2b20fda8ccb
zambia1972 <qatan.1972@gmail.com>
2025-05-31 10:01:46 +0200

    Update index.user.js

--- index.user.js
+++ index.user.js
@@ -1,22 +1,24 @@
 // ==UserScript==
 // @name         Grepolis Manager
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Volledig modulair script voor Grepolis
+// @version      0.1
+// @description  Popup met werkbalk en buttons, inclusief Afwezigheidsassistent en Militaire Manager
+// @author       Zambia1972
 // @match        *://*.grepolis.com/*
 // @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
 // @grant        unsafeWindow
-// @require      https://www.gstatic.com/firebasejs/10.11.0/firebase-app-compat.js
-// @require      https://www.gstatic.com/firebasejs/10.11.0/firebase-auth-compat.js
-// @require      https://www.gstatic.com/firebasejs/10.11.0/firebase-firestore-compat.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/utils/dom.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/chat/firebase.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/chat/parser.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/chat/toolbar.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/chat/interface.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/chat/autocomplete.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/forumManager.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/helpers/attackRangeHelper.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/modules/helpers/feestenFixedHelper.js
-// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main/core/main.js
+// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main//core/main.js
+// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main//modules/forumManager.js
+// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main//modules/helpers/attackRangeHelper.js
+// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main//modules/helpers/feestenFixedHelper.js
+// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main//modules/helpers/afwezigheidsassistent.js
+// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main//modules/militaryManager.js
+// @require      https://github.com/zambia1972/Grepolis-Manager/raw/refs/heads/main//modules/utils/dom.js
 // ==/UserScript==
+
+// Laad hoofdcomponent
+(function() {
+    'use strict';
+    new ForumManager();
+})();
