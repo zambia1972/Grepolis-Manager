494007218a2b597cdf307df984c448ca8b68a3b3
zambia1972 <qatan.1972@gmail.com>
2025-06-06 22:35:17 +0200

    Update main.js

--- src/core/main.js
+++ src/core/main.js
@@ -1,9 +1,23 @@
-// main.js - Startpunt voor Grepolis Manager
+// main.js - Hoofdbestand voor Grepolis Manager
 import { initializeButtons } from './ui.js';
 import { injectGlobalStyles } from './styles.js';
+
+import { showStartscreenPopup } from './popup.js';
+import { startAttackRangeHelper } from '../modules/attackRangeHelper.js';
+import { startFeestenManager } from '../modules/feestenManager.js';
+import { startTroopManager } from '../modules/troopManager.js';
+import { startForumManager } from '../modules/forumManager.js';
 
 (function () {
   'use strict';
   injectGlobalStyles();
-  initializeButtons();
+  initializeButtons([
+    () => showStartscreenPopup(),         // Button 1: Startscherm
+    (on) => startAttackRangeHelper(on),   // Button 2: AttackRange
+    (on) => startFeestenManager(on),      // Button 3: Feesten
+    () => console.log("Chat volgt later"),// Button 4
+    (on) => startTroopManager(on),        // Button 5: TroopManager
+    () => console.log("Kaart volgt later"),// Button 6
+    () => startForumManager()         // Button 7: ForumManager
+  ]);
 })();
