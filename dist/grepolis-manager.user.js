09367753e6cb8465482dc3563a1f2a98c4e21bb9
zambia1972 <qatan.1972@gmail.com>
2025-06-06 20:46:50 +0200

    Add files via upload

---
+++ dist/grepolis-manager.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Grepolis Manager
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Popup met werkbalk en modules
+// @author       Zambia1972
+// @match        *://*.grepolis.com/*
+// @grant        GM_addStyle
+// @grant        GM_xmlhttpRequest
+// @grant        unsafeWindow
+// ==/UserScript==
+(() => {
+})();
