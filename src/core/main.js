7b3337d1b6f4960f4ac175f12efc7b1bee6098fb
zambia1972 <qatan.1972@gmail.com>
2025-06-07 12:49:33 +0200

    Update main.js

--- src/core/main.js
+++ src/core/main.js
@@ -1,17 +1,28 @@
-
 (function () {
-  'use strict';
-  injectGlobalStyles();
-  initializeButtons([
-    () => showStartscreenPopup(),         // Button 1: Startscherm
-    (on) => startAttackRangeHelper(on),   // Button 2: AttackRange
-    (on) => startFeestenManager(on),      // Button 3: Feesten
-    () => console.log("Chat volgt later"),// Button 4
-    (on) => startTroopManager(on),        // Button 5: TroopManager
-    () => console.log("Kaart volgt later"),// Button 6
-    () => startForumManager()         // Button 7: ForumManager
-  ]);
-  window.addEventListener('load', () => {
-    initUI();  // of een andere startfunctie
-  });
+    const tryInit = () => {
+        if (
+            typeof injectGlobalStyles === 'function' &&
+            typeof initializeButtons === 'function' &&
+            typeof startAttackRangeHelper === 'function' &&
+            typeof startFeestenManager === 'function' &&
+            typeof startTroopManager === 'function' &&
+            typeof startForumManager === 'function'
+        ) {
+            injectGlobalStyles();
+
+            initializeButtons([
+                () => console.log("Startscherm volgt"),
+                (on) => startAttackRangeHelper(on),
+                (on) => startFeestenManager(on),
+                () => console.log("Chat volgt later"),
+                (on) => startTroopManager(on),
+                () => console.log("Kaart volgt later"),
+                (on) => startForumManager(on)
+            ]);
+        } else {
+            setTimeout(tryInit, 100);
+        }
+    };
+
+    tryInit();
 })();
