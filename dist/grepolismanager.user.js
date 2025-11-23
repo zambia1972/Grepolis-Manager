71008270283538dfba0d554d2a47c08036042cc7
zambia1972 <qatan.1972@gmail.com>
2025-11-23 20:04:15 +0100

    Update grepolismanager.user.js

---
+++ dist/grepolismanager.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Grepolis Manager
+// @namespace    https://github.com/zambia1972/Grepolis-Manager
+// @version      1.0.0
+// @description  Nieuwe generatie Grepolis Manager
+// @author       Hans
+// @match        https://*.grepolis.com/*
+// @grant        GM_addStyle
+// @require      https://raw.githubusercontent.com/zambia1972/Grepolis-Manager/main/src/config.js
+// @require      https://raw.githubusercontent.com/zambia1972/Grepolis-Manager/main/src/styles/styles.css
+// @require      https://raw.githubusercontent.com/zambia1972/Grepolis-Manager/main/src/core/popup.js
+// @require      https://raw.githubusercontent.com/zambia1972/Grepolis-Manager/main/src/core/ui.js
+// @require      https://raw.githubusercontent.com/zambia1972/Grepolis-Manager/main/src/main.js
+// ==/UserScript==
